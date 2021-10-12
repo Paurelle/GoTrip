d9af36444bdbc051208c7623318fda1b92278131
Paurelle <86485829+Paurelle@users.noreply.github.com>
2021-10-12 19:27:01 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'niagxsx1n1qo11uw' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'vtufk490c4z6icn7' );
+define( 'DB_USER', 'root' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'sy2t2rcucuaqzvqp' );
+define( 'DB_PASSWORD', '' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
